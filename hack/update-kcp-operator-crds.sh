28343317a589b80aec9cd4b32d355c81af6e4056
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-08-18 17:51:57 +0300

    Fix missing CRDs (#243)
    * Add missing CRDs
    
    * bump chart version
    
    * Split files for size

--- hack/update-kcp-operator-crds.sh
+++ hack/update-kcp-operator-crds.sh
@@ -5,10 +5,15 @@ cd $(dirname $0)/..
 
 cd charts/kcp-operator/
 version="$(yq '.appVersion' Chart.yaml)"
-crdFile=templates/crds.yaml
+crdFileCore=templates/crds.yaml
+crdFileCompiled=templates/crds-compiled.yaml
 
 set -x
 
-echo "{{- if .Values.crds.create }}" > "$crdFile"
-kubectl kustomize "https://github.com/kcp-dev/kcp-operator/config/crd?ref=$version" | yq >> "$crdFile"
-echo "{{- end }}" >> "$crdFile"
+echo "{{- if .Values.crds.create }}" > "$crdFileCore"
+kubectl kustomize "https://github.com/kcp-dev/kcp-operator/config/crd?ref=$version" | yq >> "$crdFileCore"
+echo "{{- end }}" >> "$crdFileCore"
+
+echo "{{- if .Values.crds.create }}" > "$crdFileCompiled"
+kubectl kustomize "https://github.com/kcp-dev/kcp-operator/config/crd/deploy?ref=$version" | yq >> "$crdFileCompiled"
+echo "{{- end }}" >> "$crdFileCompiled"
